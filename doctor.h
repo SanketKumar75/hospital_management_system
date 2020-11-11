51b6d2dfee941ee8e39f466139e49f4c54690db7
SanketKumar75 <63141922+SanketKumar75@users.noreply.github.com>
2020-11-11 19:23:48 +0530

    Update doctor.h

--- doctor.h
+++ doctor.h
@@ -1,5 +1,7 @@
 //doctor.h
 #include<iostream>
+#include<fstream>
+#include<string>
 using namespace std;
 
 class doctor{
@@ -15,8 +17,12 @@ class doctor{
 
     int init(){
     cout<<"This too!"<<endl;
-    cout<<"Enter your choice"<<endl;
-    cout<<"<1>List Of Doctors"<<endl<<"<2>Check Availability"<<endl<<"<3>Add A New Doctor"<<endl<<"<9>Go Back TO Main Menu"<<endl<<"<0>To Exit"<<endl;
+    cout<<"Enter your choice"<<endl; 
+    cout<<"<1>List Of Doctors"<<endl
+        <<"<2>Check Availability"<<endl
+        <<"<3>Add A New Doctor"<<endl
+        <<"<9>Go Back TO Main Menu"
+        <<endl<<"<0>To Exit"<<endl;
     cin>>c;
     switch(c){
         case 0:
@@ -39,6 +45,7 @@ class doctor{
         case 3:
             {
                 cout<<"add new\n";
+                i=add_doc();
                 break;
             }
         case 9:
@@ -51,18 +58,50 @@ class doctor{
             }
         
     }
-    return 1;
+    return i;
 }
 };
 
 int doctor::list_doc(){
     cout<<"printing list of doctor\n";
+    
+
+    return 0;
 }
 
 int doctor::avail(){
     cout<<"checking availability";
+    return 0;
 }
 
 int doctor::add_doc(){
-    cout<<"adding doctor";
+    cout<<"adding doctor\n";
+    fstream fin;
+
+    fin.open("test.cvs", ios::out | ios::app);
+
+    string dname,catagory;
+    int id,r;
+    cout<<"Enter doctor's first name: "<<endl;
+    cin>>dname;
+    cout<<"Enter doctors ID no. : "<<endl;
+    cin>>id;
+    cout<<"The doctor has specialisation in: "<<endl;
+    cin>>catagory;
+
+    fin<<id<<", "<<dname<<", "<<catagory<<"\n";
+    cout<<"Updated successfully!!\n";
+    
+    cout<<"Do you want to add another doctor?< 1=yes & 0=no>\n";
+    cin>>r;
+    if(r==0)
+        return r+1;
+    else if(r==1)
+        add_doc();
+    
+
+
+
+
+    return 0;
 }
