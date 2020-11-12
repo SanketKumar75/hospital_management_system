631673a6324e9b3520c3fa75c7be9d248ab1558d
SanketKumar75 <63141922+SanketKumar75@users.noreply.github.com>
2020-11-13 01:16:39 +0530

    Update doctor.h
    turns out csv koi kaam ka nai he 
    txt file ne ik dum takdeer badal die jazbaad badal die zindagi badal di

--- doctor.h
+++ doctor.h
@@ -9,18 +9,27 @@ class doctor{
         int id=123;
         int c;
         int i;
-        char name;
+        int age;
+        int experience;
+        string fname;
+        string lname;
+        string qual;
+        string city;
+
         char cat;
-        int list_doc();
+        int doc_info();
         int avail();
         int add_doc();
+        string getSpec();
+
 
     int init(){
     cout<<"This too!"<<endl;
     cout<<"Enter your choice"<<endl; 
-    cout<<"<1>List Of Doctors"<<endl
+    cout<<"<1>Information Of a Doctor"<<endl
         <<"<2>Check Availability"<<endl
         <<"<3>Add A New Doctor"<<endl
+        <<"<4>List Of a All Doctor"<<endl
         <<"<9>Go Back TO Main Menu"
         <<endl<<"<0>To Exit"<<endl;
     cin>>c;
@@ -32,8 +41,8 @@ class doctor{
             }
         case 1:
             {
-                cout<<"list of doc\n";
-                i=list_doc();
+                cout<<"Info of doctor\n";
+                i=doc_info();
                 break;
             }
         case 2:
@@ -47,6 +56,10 @@ class doctor{
                 cout<<"add new\n";
                 i=add_doc();
                 break;
+            }
+        case 4:
+            {
+                cout<<"List of Doctor\n";
             }
         case 9:
             {
@@ -62,9 +75,48 @@ class doctor{
 }
 };
 
-int doctor::list_doc(){
+int doctor::doc_info(){
     cout<<"printing list of doctor\n";
-    
+    {
+    int x;
+    cout<<"\n\nEnter the ID of the Doctor whose profile you want to display:"<<endl;
+    cin>>x;									 //ID,firstname,lastname,age,qualification,specialization,experience,city
+	ifstream file;file.open("doctor.txt",ios::out|ios::in);
+	file.seekg(0);
+		
+	int ID,age,exp,count=0;
+	string fname,lname,ci,qua,spe;
+	
+//	 if (file.is_open())
+// 		 cout << "File successfully open";
+//	else
+//    cout << "Error opening file";
+  
+	while(file >> ID >> fname >> lname >>age >>qua >> spe >>exp >> ci)   //!fileObj.eof()
+	{
+	
+		if(ID==x)
+		{
+		cout<<"\n\n1.ID:"<<ID<<endl<<"2.First Name:"<<fname<<endl<<"3.Last Name:"<<lname<<endl<<"4.Age:"<<age<<endl<<"5.Qualification:"<<qua<<endl<<"6.Specialization:"<<spe<<endl<<"7.Experience:"<<exp<<endl<<"8.City:"<<ci<<endl<<endl;
+		count++;
+		}
+	}
+	file.close();
+	if (count==0)
+			cout<<"\nNo matching records found!"<<endl;
+			
+	cout<<"\n\nEnter 1 to return to Doctor Database\nEnter 2 to Exit."<<endl;
+	int c;
+	cin>>c;
+	if(c==1){
+	  system("cls");
+	  init();
+	}
+	  
+	else
+	  exit(0);
+
+}
 
     return 0;
 }
@@ -75,7 +127,7 @@ int doctor::avail(){
 }
 
 int doctor::add_doc(){
-    cout<<"adding doctor\n";
+    /*cout<<"adding doctor\n";
     fstream fin;
 
     fin.open("test.cvs", ios::out | ios::app);
@@ -103,5 +155,100 @@ int doctor::add_doc(){
 
 
 
-    return 0;
+    return 0;*/
+
+	    cout<<"Fill up the following details"<<endl;
+	    cout<<"ID:"<<endl;
+	    cin>>id;
+   		cout<<"First Name:"<<endl;
+   		cin>>fname;
+   		cout<<"Last Name:"<<endl;
+   		cin>>lname;
+   		cout<<"Age:"<<endl;
+   		cin>>age;
+   		cout<<"Choose Qualification among the following:\n1.MBBS\n2.MD"<<endl;
+   		int q;
+   		cin>>q;
+   		if(q==1)
+   			qual="MBBS";
+		else qual="MD";
+		cout<<"Choose Specialization among the following:"<<endl;
+		string spec=getSpec();
+		cout<<"Experience (in Years):"<<endl;
+		cin>>experience;
+		cout<<"City:"<<endl;
+		cin>>city;
+		system("cls");
+		
+		
+		fstream fileObj;   
+		fileObj.open("doctor.txt",ios::in | ios::app );
+
+        fileObj.seekp (0, ios::end);
+		fileObj<<id<<endl<<fname<<endl<<lname<<endl<<age<<endl<<qual<<endl<<spec<<endl<<experience<<endl<<city<<endl;
+		fileObj.close();
+	
+	//after completion
+	cout<<"\n\t\tNew Doctor profile created successfully!"<<endl<<endl<<endl;
+	cout<<"\n\n\n\nEnter 1 to return to Doctor Database\nEnter 2 to Exit."<<endl;
+	int c;
+	cin>>c;
+	if(c==1){
+	  system("cls");
+	  init();
+	}
+	  
+	else
+	  exit(0);
 }
+
+
+
+
+
+ string doctor::getSpec(){
+ 	string sp;
+	cout<<"1.GENERAL_MEDICINE"<<endl;
+	cout<<"2.NEUROLOGY"<<endl;
+	cout<<"3.ONCOLOGY"<<endl;
+	cout<<"4.HAEMATOLOGY"<<endl;
+	cout<<"5.GASTROENTEROLOGY"<<endl;
+	cout<<"6.GYNAECOLOGY "<<endl;
+	int s;
+	cin>>s;
+	switch(s){
+		
+		case 1:{
+			sp= "GENERAL_MEDICINE";
+			break;
+		}
+		case 2:{
+			sp="NEUROLOGY";
+			break;
+		}
+		case 3:{
+		    sp="ONCOLOGY";
+			break;
+		}
+		case 4:{
+			sp= "HAEMATOLOGY";
+			break;
+		}
+		case 5:{
+			sp= "GASTROENTEROLOGY";
+			break;
+		}
+		case 6:{
+			sp= "GYNAECOLOGY ";
+			break;
+		}
+		default:{
+			cout<<"\nCome on!Enter a valid option."<<endl;
+			
+		}
+	
+	}
+		return sp;
+	
+}
+
