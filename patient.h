5e26bb0ed7265cfb72a2fb06c771d3d145218707
SanketKumar75 <63141922+SanketKumar75@users.noreply.github.com>
2020-11-15 02:40:20 +0530

    Update patient.h

--- patient.h
+++ patient.h
@@ -12,7 +12,10 @@ class patient{
         int c;
         int i;
         int age;
-
+		int doc_fee;
+		int bed_charge;
+		int mbill;
+		int tbill;
         string phno;
         string in_date;
         string fname;
@@ -26,6 +29,10 @@ class patient{
 			patient(){
 					id=0;
 					age=0;
+					doc_fee=2000;
+					mbill=0;
+					bed_charge=4500;
+					tbill=doc_fee + bed_charge;
 					fname="NoInput";
 					lname="NoInput";
 					occupation="NotDefined";
@@ -50,7 +57,7 @@ class patient{
         <<"<2>Display A Patient's Information"<<endl
         <<"<3>List Of All Patients"<<endl
         <<"<4>Patient Discharge"<<endl
-        <<"<5>Add to bill(Medical store bill)"<<endl
+        <<"<5>Print Reciept"<<endl
         <<"<9>Go Back TO Main Menu"<<endl
         <<"<0>To Exit"<<endl;
     cin>>c;
@@ -81,12 +88,13 @@ class patient{
         case 4:
             {
                 cout<<"Discharge patient\n";
+				system("cls");
                 i=discharge();
                 break;
             }
         case 5:
             {
-                cout<<"Add to bill(Medical store bills)\n";
+                cout<<"Print reciept\n";
                 i=bill();
                 break;
             }
@@ -249,14 +257,107 @@ cout<<"\n\nEnter 1 to return to Patient Database\nEnter 2 to Exit."<<endl;
 
 int patient::discharge(){
     cout<<"patient checkout"<<endl;
+		int x;
+	cout<<"\nEnter the ID of the Patient who is being discharged: "<<endl;
+	cin>>x;									 
+	fstream patObj("patient.txt");
+	patObj.seekg(0);
+													//ID,firstname,lastname,age,phnumber,occupation,city,disease,Adate,Rdate,bill,dept
+	string fname,lname,occupation,p_city,dis,addDate,dept;
+	int p_age,p_ID,count=0;
+	long double phnumber;
+	while(patObj>>p_ID>>fname>>lname>>p_age>>phnumber>>occupation>>p_city>>dis>>addDate>>dept)
+	{
+		if(p_ID==x){
+		cout<<"1.ID:"<<p_ID<<endl<<"2.First Name:"<<fname<<endl
+			<<"3.Last Name:"<<lname<<endl<<"4.Age"<<p_age<<endl
+			<<"5.Phone No.:"<<phnumber<<endl<<"6.Occupation:"<<occupation<<endl
+			<<"7.City:"<<p_city<<endl<<"8.Disease/Symptoms:"<<dis<<endl
+			<<"9.Admission date(DD/MM/YYYY):"<<addDate<<endl
+			<<"12.Kind of Specialization required:"<<dept<<endl<<endl;
+		count++;
+		break;
+		}
+	}
+	
+	patObj.close();
+	if(count==0){
+		cout<<"\nNo matching records found!"<<endl;
+		exit(0);
+	}
+	cout<<"Patients Medical Reciept: "<<endl;
+		int tbill=bill();
+		cout<<"The total amount to be paid is: "<<tbill<<endl;
+	cout<<"Discharge "<<fname<<" "<<lname<<" ?\n<1>YES <2>NO"<<endl;
+	int n;cin>>n;
+	if(n==1)
+	{
+	fstream file("patient.txt");
+	file.seekg (0, ios::beg);
+	file.seekg(0);
+	int line;
+    ofstream tempObj;
+	tempObj.open("copy2.txt");
+    tempObj.seekp(0);
+  	int skip = 0;
+  	string fname1,lname1,occupation1,p_city1,dis1,addDate1,relDate1,dept1;
+	int p_age1;
+	long double phnumber1,bill1;
+	file.seekg(0);file.seekg(0,ios::cur); //setting the pointer to beginning of file
+		
+//	 if (tempObj.is_open())
+// 		 cout << "File successfully open";
+//	else
+//    	cout << "Error opening file";
+	while(file>>line>>fname1>>lname1>>p_age1>>phnumber1>>occupation1>>p_city1>>dis1>>addDate1>>dept1)
+	{
+		if(line!=x){
+			tempObj<<line<<endl<<fname1<<endl<<lname1<<endl<<p_age1<<endl<<phnumber1<<endl<<occupation1<<endl<<p_city1<<endl<<dis1<<endl<<addDate1<<endl<<dept1<<endl<<endl;
+		}			
+	}
 
-    return 0;
+  file.close();
+  tempObj.close();
+  remove("patient.txt");
+  int result;
+  char oldname[] ="copy2.txt";
+  char newname[] ="patient.txt";
+  result= rename( oldname , newname );
+  system("cls");
+  cout<<"\n\t\tThe patient was discharged"<<endl;
 }
 
-int patient::bill(){
-    cout<<"add to bill"<<endl;
+cout<<"\n\nEnter 1 to return to Patient Database\nEnter 2 to Exit."<<endl;
+	int c;
+	cin>>c;
+	if(c==1){
+	  system("cls");
+	  i=init();
+	  return i;
+	}
+	  
+	else
+	  exit(0);
+}
 
-    return 0;
+
+int patient::bill(){
+    cout<<"Printing reciept.."<<endl;
+
+	cout<<"Doctors Fee: "<<doc_fee<<endl
+		<<"Bed Charges: "<<bed_charge<<endl;
+	cout<<"Was there any medical store bill? \n<1>Yes <2>No"<<endl;
+	cin>>c;
+	if(c==1){
+		cout<<"Enter total amount: ";
+		cin>>mbill;
+	}
+	cout<<"Doctors Fee: "<<doc_fee<<endl
+		<<"Bed Charges: "<<bed_charge<<endl
+		<<"Medication Bill: "<<mbill<<endl;
+		cout<<"total: "<<doc_fee + bed_charge + mbill<<endl;
+
+	return doc_fee + bed_charge + mbill;
 }
 
 
